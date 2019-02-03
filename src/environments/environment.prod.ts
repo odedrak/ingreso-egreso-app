576fc2d8c9f2af693003dfd8ccba432ee1c544c7
odedrak <odedrak@hotmail.com>
2019-02-03 15:47:56 +0100

    Sección 7 lista

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCP2JD5VqIkGAzD99RzVnKXQEhFj6685BA',
+    authDomain: 'ingreso-egreso-app-3bea4.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-3bea4.firebaseio.com',
+    projectId: 'ingreso-egreso-app-3bea4',
+    storageBucket: 'ingreso-egreso-app-3bea4.appspot.com',
+    messagingSenderId: '70216024848'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCP2JD5VqIkGAzD99RzVnKXQEhFj6685BA',
+    authDomain: 'ingreso-egreso-app-3bea4.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-3bea4.firebaseio.com',
+    projectId: 'ingreso-egreso-app-3bea4',
+    storageBucket: 'ingreso-egreso-app-3bea4.appspot.com',
+    messagingSenderId: '70216024848'
+  }
 };
 
 /*
