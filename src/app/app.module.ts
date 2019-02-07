da957b8168daa549754fce9cf00d29c5c3c74565
odedrak <odedrak@hotmail.com>
2019-02-07 18:58:07 +0100

    Sección 8 lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+
+// NGRX
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducer';
+
+
 // Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -20,6 +27,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { environment } from '../environments/environment';
+
 
 
 @NgModule({
@@ -41,7 +49,12 @@ import { environment } from '../environments/environment';
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,12 @@ import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { map } from 'rxjs/operators';
 import { User } from './user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
+
 
 
 @Injectable({
@@ -12,15 +18,28 @@ import { User } from './user.model';
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth, private router: Router, private afDB: AngularFirestore) { }
+  private userSubscription: Subscription = new Subscription();
+
+  constructor(private afAuth: AngularFireAuth, private router: Router, private afDB: AngularFirestore, private store: Store<AppState>) { }
 
   initAuthListener() {
     this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
-
+      if (fbUser) {
+        this.userSubscription = this.afDB.doc(`${fbUser.uid}/usuario`).valueChanges().subscribe( (usuarioObj: any) => {
+            const newUser = new User(usuarioObj);
+            this.store.dispatch(new SetUserAction(newUser));
+        });
+      } else {
+        // Si el usuario es nulo, se cancela la subscripcion actual del usuario
+        this.userSubscription.unsubscribe();
+      }
     });
   }
 
   crearUsuario( nombre: string, email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then( resp => {
           const user: User = {
@@ -32,21 +51,27 @@ export class AuthService {
           this.afDB.doc(`${user.uid}/usuario`).set(user)
               .then( () => {
                 this.router.navigate(['/']);
+                this.store.dispatch(new DesactivarLoadingAction());
               });
 
 
       }).catch( error => {
+        this.store.dispatch(new DesactivarLoadingAction());
         Swal.fire('Error en el login', error.message, 'error');
     });
 
   }
 
   login(email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then( resp => {
-          console.log(resp);
+          this.store.dispatch(new DesactivarLoadingAction());
           this.router.navigate(['/']);
       }).catch( error => {
+        this.store.dispatch(new DesactivarLoadingAction());
         Swal.fire('Error en el login', error.message, 'error');
     });
   }
